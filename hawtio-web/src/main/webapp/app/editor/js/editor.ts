7c73001c2ad342a7e1ebdcb6c6b650094c2e7acb
Charles Moulliard <ch007m@gmail.com>
2014-03-18 15:51:47 +0100

    Add a select option to choice a theme

--- hawtio-web/src/main/webapp/app/editor/js/editor.ts
+++ hawtio-web/src/main/webapp/app/editor/js/editor.ts
@@ -5,38 +5,37 @@ module Editor {
 
     export function TodoCtrl($scope) {
         $scope.todos = [
-            {text:'learn angular', done:true},
-            {text:'build an angular app', done:false}];
+            {text: 'learn angular', done: true},
+            {text: 'build an angular app', done: false}
+        ];
 
-        $scope.addTodo = function() {
-            $scope.todos.push({text:$scope.todoText, done:false});
+        $scope.addTodo = function () {
+            $scope.todos.push({text: $scope.todoText, done: false});
             $scope.todoText = '';
         };
 
-        $scope.remaining = function() {
+        $scope.remaining = function () {
             var count = 0;
-            angular.forEach($scope.todos, function(todo) {
+            angular.forEach($scope.todos, function (todo) {
                 count += todo.done ? 0 : 1;
             });
             return count;
         };
 
-        $scope.archive = function() {
+        $scope.archive = function () {
             var oldTodos = $scope.todos;
             $scope.todos = [];
-            angular.forEach(oldTodos, function(todo) {
+            angular.forEach(oldTodos, function (todo) {
                 if (!todo.done) $scope.todos.push(todo);
             });
         };
     }
 
-    export function RenderCtrl(asciidocOpts) {
+    export function RenderCtrl($scope) {
 
-        var app = this;
-        app.asciidocOpts = asciidocOpts;
-
-        app.ascii= '== Write some text or drop an AsciiDoc file in editor area\n';
-        app.ascii+= '= <-';
+        // The Themes
+        $scope.themes = ['chrome', 'twilight', 'terminal', 'solarized_dark', 'solarized_light','textmate'];
+        $scope.theme = $scope.themes[3];
 
         /**
          * Define Post processor to change html generated with asciidoc
@@ -46,41 +45,77 @@ module Editor {
         var urlImages = 'https://raw2.github.com/asciidoctor/asciidoctor.js/master/examples/';
         var urlLink = 'https://github.com/Nikku/asciidoc-samples/blob/master/';
 
-        app.asciiPostProcessor = function(element) {
+        $scope.asciiPostProcessor = function(element) {
             element.find('a').not('[href^="http"]').each(function() {
                 var el = angular.element(this)
                 var href = el.attr('href');
                 el.attr('href', urlLink+href)
             });
-
             element.find('img').not('[src^="http"]').each(function() {
                 var el = angular.element(this);
                 var srcImg = el.attr('src');
                 el.attr('src',  urlImages+srcImg);
             });
-
             return element;
         }
 
         // The ui-ace option
-        app.aceOption = {
-            theme:'terminal',
-            mode: 'asciidoc'
-        }
+        $scope.aceOption = {
+            mode:     'asciidoc',
+            theme:    $scope.theme.toLowerCase(),
+            fontsize: '16px',
+            onLoad: function (acee) {
+                $scope.themeChanged = function () {
+                    acee.setTheme('ace/theme/' + $scope.theme.toLowerCase());
+                };
+            }
+        };
+
+        $scope.ascii=  '== Write some text or drop an AsciiDoc file in editor area\n';
+        $scope.ascii+= '== <-';
 
 
-
-        app.onDragOver = function(event) {
+        $scope.onDragOver = function (event) {
             console.log(event);
         };
 
-        app.onDragEnter = function(event) {
+        $scope.onDragEnter = function (event) {
             console.log(event);
         };
 
-        app.onDragLeave = function(event) {
+        $scope.onDragLeave = function (event) {
             console.log(event);
         };
+    }
+
+    export function AceCtrl($scope) {
+
+        // The modes
+        $scope.modes = ['Scheme', 'XML', 'Javascript'];
+        $scope.mode = $scope.modes[0];
+
+
+        // The ui-ace option
+        $scope.aceOption = {
+            mode: $scope.mode.toLowerCase(),
+            onLoad: function (_ace) {
+
+                // HACK to have the ace instance in the scope...
+                $scope.modeChanged = function () {
+                    _ace.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
+                };
+
+            }
+        };
+
+        // Initial code content...
+        $scope.aceModel = ';; Scheme code in here.\n' +
+            '(define (double x)\n\t(* x x))\n\n\n' +
+            '<!-- XML code in here. -->\n' +
+            '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
+            '// Javascript code in here.\n' +
+            'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}';
+
     }
 
 
